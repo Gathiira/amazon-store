53e4a75ee8d5175cdd80bfadc09043ac347ed85e
Gathiira <mwangyjose@gmail.com>
2021-03-30 08:27:23 +0300

    bugfix: product component to have greater z index

--- src/Home.js
+++ src/Home.js
@@ -16,6 +16,58 @@ function Home() {
                     price={1200} 
                     rating={4} 
                     image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                </div>
+                <div className="home__row">
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                </div>
+                <div className="home__row">
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+                    <Product 
+                    id= {1}
+                    title="Macbook pro Air max 13.3 inches. Very clean" 
+                    price={1200} 
+                    rating={4} 
+                    image="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
                 </div>
             </div>
         </div>
